f15c2bb92bd47e8e4705eda1819cf716b71f9c20
bryamdmg <bryamdmg@gmail.com>
2025-03-16 23:06:22 -0600

    register created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,9 @@ export const routes: Routes = [
       path: "login",
       component: LoginComponent
     },
+    {
+      path: "register",
+      component: RegisterComponent
+    }
 ];
 export default routes;
