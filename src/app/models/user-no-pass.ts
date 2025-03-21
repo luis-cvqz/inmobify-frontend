19f53af59281d3aa7d6046dc3002baa3fe6e3770
Camilo <camiloesan@gmail.com>
2025-03-20 19:09:22 -0600

    get user component / needs testing with auth

---
+++ src/app/models/user-no-pass.ts
@@ -0,0 +1,9 @@
+export interface UserNoPass {
+  id: string;
+  name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  created_at: string;
+  user_type_id: number;
+}
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { UserNoPass } from "../models/user-no-pass";
+
+@Injectable({
+  providedIn: "root",
+})
+export class UsersService {
+  private url = "http://localhost:12000/user";
+
+  constructor() {}
+
+  async fetchUser(userId: string): Promise<UserNoPass> {
+    const token = localStorage.getItem("jwt_token") || "";
+    const response = await fetch(`${this.url}/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) throw new Error(`HTTP error status: ${response.status}`);
+
+    return await response.json();
+  }
+}
