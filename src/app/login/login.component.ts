8df6c2fcdd494eeb126d7aa271ba7aa71a09e7aa
bryamdmg <bryamdmg@gmail.com>
2025-04-11 22:05:57 -0600

    delete css on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule, RouterModule
+    FormsModule, RouterModule, NgOptimizedImage
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
