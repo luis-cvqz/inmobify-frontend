4ba89e3b0f089d2d5ba3eed240aa6e68d022961a
bryamdmg <bryamdmg@gmail.com>
2025-05-22 23:30:52 -0600

    add sections for properties types and disposition

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,15 +27,74 @@ export class HomeComponent {
   ];
   filteredCities: string[] = [];
 
+  selectedFilter: string = "Todos";
+
   propertiesService: PropertiesService = inject(PropertiesService);
+
+  private allProperties: PropertySummary[] = [];
   propertyList: PropertySummary[] = [];
 
+  readonly pageSize = 20;
+  currentPage = 0;
+
   constructor() {
-    this.propertiesService
-      .getProperties()
-      .then((propertyList: PropertySummary[]) => {
-        this.propertyList = propertyList;
-      });
+    this.loadProperties();
+  }
+
+  async loadProperties() {
+    this.allProperties = await this.propertiesService.getProperties();
+    this.loadNextPage();
+  }
+
+  applyFilterAndPagination(resetPage: boolean = true) {
+    if (resetPage) this.currentPage = 0;
+    this.propertyList = [];
+
+    let filtered = this.filterProperties(this.allProperties);
+
+    this.propertyList = filtered.slice(0, this.pageSize);
+    this.currentPage = 1;
+  }
+
+  loadNextPage() {
+    const filtered = this.filterProperties(this.allProperties);
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    this.propertyList = this.propertyList.concat(filtered.slice(start, end));
+    this.currentPage++;
+  }
+
+  hasMoreProperties(): boolean {
+    const filtered = this.filterProperties(this.allProperties);
+    return this.propertyList.length < filtered.length;
+  }
+
+  filterProperties(properties: PropertySummary[]): PropertySummary[] {
+    switch (this.selectedFilter) {
+      case "Residencias (Venta)":
+        return properties.filter(
+          (p) => p.property_type === "residential" && p.disposition === "sale"
+        );
+      case "Residencias (Renta)":
+        return properties.filter(
+          (p) => p.property_type === "residential" && p.disposition === "lease"
+        );
+      case "Locales comerciales (Venta)":
+        return properties.filter(
+          (p) => p.property_type === "commercial" && p.disposition === "sale"
+        );
+      case "Locales comerciales (Renta)":
+        return properties.filter(
+          (p) => p.property_type === "commercial" && p.disposition === "lease"
+        );
+      default:
+        return properties;
+    }
+  }
+
+  changeFilter(filter: string) {
+    this.selectedFilter = filter;
+    this.applyFilterAndPagination(true);
   }
 
   onSearch() {
