4381e7143d61d9d01395818c115cc8c8c0c089da
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-10-14 23:37:53 -0600

    add environment configuration file example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: "api_key",
+  appointmentsUrl: "http://IP_ADDRESS/imf-appointments",
+  authUrl: "http://IP_ADDRESS/imf-auth",
+  propertiesUrl: "http://IP_ADDRESS/imf-properties",
+  fileServerUrl: "http://IP_ADDRESS/imf-files",
+  usersUrl: "http://IP_ADDRESS/imf-users/users"
+};
